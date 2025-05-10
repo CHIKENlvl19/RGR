556581f8bf1f5d95b9b3453c79084709ab2b88ed
bloodfallen <74541116+CHIKENlvl19@users.noreply.github.com>
2025-05-10 18:47:16 +0700

    started developing the app, made menu and password check

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "header.h"
 
 using namespace std;
 
 int main() {
+
+    PasswordCheck();
+    Greeting();
+
 
     return 0;
 }
---
+++ menu.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <sstream>
+#include "header.h"
+
+using namespace std;
+
+void PasswordCheck() {
+    cout << "Для начала работы с приложением введите пароль.\n";
+    int password = 199672;
+    int password_input;
+
+    int attempts = 0;
+    while (attempts < 10)
+    {
+        cout << "Пароль: ";
+        cin >> password_input;
+        if (password == password_input)
+        {
+            cout << "Пароль верный. Доступ разрешен.\n\n";
+            break;
+        }
+        else
+        {
+            cout << "Пароль неверный, осталось попыток: " << 10 - attempts - 1 << endl;
+            attempts++;
+        }
+
+        if (attempts == 10)
+        {
+            cout << "Превышен лимит попыток. Доступ запрещен.\n";
+            exit(0);
+        }
+    }
+
+}
+
+void Greeting() {
+    cout << "Добро пожаловать в программу шифрования и дешифровки текста! Введите, пожалуйста, текстом, метод ввода: \n\n";
+    cout << "Доступные методы ввода: \n";
+    cout << "• С клавиатуры\n";
+    cout << "• Из файла\n";
+
+    
+}
+
